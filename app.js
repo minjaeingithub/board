ff24239f72b67f99eb5902311cb0c771e17b485b
daniel2231 <kangheon2231@gmail.com>
2021-01-16 16:57:27 +0900

    CRUD

--- app.js
+++ app.js
@@ -14,6 +14,26 @@ mongoose.connect("mongodb://localhost:27017/todolistDB", {
   useNewUrlParser: true,
 });
 
+const itemsSchema = {
+  name: String,
+};
+
+const Item = mongoose.model("item", itemsSchema);
+
+const item1 = new Item({
+  name: "Welcome to your todolist",
+});
+
+const item2 = new Item({
+  name: "Hit the + button to add a new item",
+});
+
+const item3 = new Item({
+  name: "<-- Hit this to delete an item",
+});
+
+const defaultItems = [item1, item2, item3];
+
 app.get("/", function (req, res) {
   Item.find({}, (err, foundItems) => {
     if (foundItems.length === 0) {
