b0e2df192e1794210c0f854d72db0d1d4dd4466e
daniel2231 <kangheon2231@gmail.com>
2021-01-16 18:59:57 +0900

    final commit

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ let app = express();
 
 app.set("view engine", "ejs");
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }, { useUnifiedTopology: true }));
 app.use(express.static("public"));
 
 mongoose.connect("mongodb://localhost:27017/todolistDB", {
@@ -17,10 +17,15 @@ mongoose.connect("mongodb://localhost:27017/todolistDB", {
 const itemsSchema = {
   name: String,
 };
+
 /*아이템스키마를 몬구스모델에 복사*/    
 const Item = mongoose.model("item", itemsSchema);
 
 /*구현하려는 기능 함수 전 변수(3개) 설정*/
+const item4 = new Item({
+  name: "alksdjfl;akjsl;fkj",
+})
+
 const item1 = new Item({
   name: "Welcome to your todolist",
 });
@@ -33,14 +38,14 @@ const item3 = new Item({
   name: "<-- Hit this to delete an item",
 });
 
-const defaultItems = [item1, item2, item3];
+const defaultItems = [item1, item2, item3, item4];
 
 //{} 모르겠씀, 함수 내용: error 구분
 app.get("/", function (req, res) {
   Item.find({}, (err, foundItems) => {
     if (foundItems.length === 0) {
-      Item.insertMany(defaultItems, function (err) {
-        if (err) {
+      Item.insertMany(defaultItems, function(err) {
+      if (err) {
           console.log(err);
         } else {
           console.log("Success");
@@ -49,7 +54,7 @@ app.get("/", function (req, res) {
       //성공하면 board.js(디폴트 화면)로 보내기
       res.redirect("/");
     } else { //투데이??
-      res.render("board", { listTitle: "Today", newListItems: foundItems });
+      res.render("board", {newListItems: foundItems});
     }
     if (err) {
       console.log(err);
@@ -68,6 +73,7 @@ app.post("/", function (req, res) {
 
   res.redirect("/"); //db에 세이브하고 원래대로 respond
 });
+
 //check하면 딜리트 페이지로 보낸다!
 app.post("/delete", function (req, res) {
   const checkedItemID = req.body.checkbox; 
@@ -78,14 +84,6 @@ app.post("/delete", function (req, res) {
       res.redirect("/");//그리고 다시 원래페이지로 커몬
     }
   });
-});
-//워크 페이지로 보낸다 => 보드 제이에스를 브라우어저에 렌더링!
-app.get("/work", function (req, res) {
-  res.render("board", { listTitle: "Work List", newListItems: workItems });
-});
-//어바웃?페이지가 뭐였더라
-app.get("/about", function (req, res) {
-  res.render("about");
 });
 
 app.listen(3000, function () {
